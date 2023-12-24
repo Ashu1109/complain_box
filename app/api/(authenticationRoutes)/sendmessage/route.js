b8bdc83f5f7dae022f2186fd35dd95f0597bc18c
Aayush kumar <aayushkumarhigh@gmail.com>
2023-12-24 20:43:07 +0530

    remove trial route

--- app/api/(authenticationRoutes)/sendmessage/route.js
+++
@@ -1,8 +0,0 @@
-import { sendMessage } from "@/app/helper/sendMesage";
-import { NextResponse } from "next/server";
-export async function GET() {
-  try {
-    const res = await sendMessage("hih", "jkj", "kkjkj");
-    return NextResponse.json({ message: "message send", res }, { status: 200 });
-  } catch (error) {}
-}
