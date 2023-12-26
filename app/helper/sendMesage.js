1f7799214a2f47124426b9df8f7a5cdf04cae751
Aayush kumar <aayushkumarhigh@gmail.com>
2023-12-26 16:44:33 +0530

    message fixes

--- app/helper/sendMesage.js
+++ app/helper/sendMesage.js
@@ -1,14 +1,14 @@
-const accountSid = process.env.Account_SID;
+const accountSid = "AC444d7701bd5228955327b123acaeed99";
 const authToken = process.env.Auth_Token;
 import twilio from "twilio";
 export async function sendMessage(flatno, title, complain) {
+  const client = twilio(accountSid, authToken);
   try {
-    const client = twilio(accountSid, authToken);
     const message = `\nNew complain\nFlat-no:${flatno}\nTitle:${title}\nComplain:${complain}`;
     const res = await client.messages.create({
       body: message,
       from: "+12818452668",
-      to: "+918210510822",
+      to: process.env.TWILIO_NUMBER,
     });
     return res;
   } catch (error) {
